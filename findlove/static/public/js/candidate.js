5edb1c7ca2de0809cd784e42d314e7ccadbac5a2
makao <2016333261@qq.com>
2016-09-21 22:14:48 +0800

    adjust the personal information layout

--- findlove/static/public/js/candidate.js
+++ findlove/static/public/js/candidate.js
@@ -8,9 +8,10 @@ $(function(){
 		}
 	});
 
-	var imglist = ["/static/public/img/1-th.jpg","/static/public/img/2-th.jpg",
-	               "/static/public/img/3-th.jpg","/static/public/img/4-th.jpg",
-	               "/static/public/img/5-th.jpg","/static/public/img/1-th.jpg"];
+	var imglist = ["http://www.xipwang.cn/photoview/imgs/1.jpg","http://www.xipwang.cn/photoview/imgs/2.jpg",
+	               "http://www.xipwang.cn/photoview/imgs/3.jpg","http://www.xipwang.cn/photoview/imgs/4.jpg",
+	               "http://www.xipwang.cn/photoview/imgs/5.jpg","http://www.xipwang.cn/photoview/imgs/6.jpg",
+	               "http://www.xipwang.cn/photoview/imgs/7.jpg"];
 	$.gallery({parent:'#photos',imgs:imglist});//加载相片列表
 	$("#PGalleryTwoC").photoview(); //生成图片预览模式
 })
